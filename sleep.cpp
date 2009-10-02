dbfc3a67b6ae681e575a72c61d4f21701cc6c7aa
Ivan Novikov <nia.fti@mail.ru>
2009-10-02 21:48:40 +0700

    Changed order of functions in .cpp, and added checking whether the parameter is numeric

--- sleep.cpp
+++ sleep.cpp
@@ -1,16 +1,5 @@
 #include "sleep.h"
 
-void usage()
-{
-    printf("Usage: sleep number\nExample: sleep 5");
-}
-
-void sleep(const char *arg)
-{
-    int n = atoi(arg);
-    Sleep(n*1000);
-}
-
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -18,4 +7,34 @@ int main(int argc, char *argv[])
     else
         sleep(argv[1]);
     return (0);
+}
+
+bool is_integer(const char *str)
+{
+    size_t length = strlen( str );
+    for( size_t i = 0; i < length; ++i )
+        if( !isdigit(str[i]) && !isspace(str[i]) )
+            return false;
+    return true;
+}
+
+void sleep(const char *arg)
+{
+    int n = atoi(arg);
+    if( n == 0 && !is_integer(arg) )
+        wrong_parameter( arg );
+    Sleep(n*1000);
+}
+
+// Printing messages:
+
+void usage()
+{
+    printf("Usage: sleep number\nExample: sleep 5");
+}
+
+void wrong_parameter(const char *param)
+{
+    printf("sleep: wrong time interval value: `%s'", param);
+    // TODO: print 'try --help blah blah blah'
 }
--- sleep.h
+++ sleep.h
@@ -2,3 +2,11 @@
 
 #include <windows.h>
 #include <cstdio>
+
+bool is_integer(const char *str);       // Checks whether given string contains an integer number
+void sleep(const char *arg);            // Parses argument and sleeps
+
+// Printing messages:
+
+void usage();                           // Prints usage message
+void wrong_parameter(const char *param);// Prints wrong parameter message
