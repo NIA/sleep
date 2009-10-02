46946a67bfecb6b6539caf2dd3b3a0c66fa43614
Ivan Novikov <nia.fti@mail.ru>
2009-10-02 21:29:04 +0700

    Added simple command-line argument handling. No error checking yet

--- sleep.cpp
+++ sleep.cpp
@@ -1,6 +1,21 @@
 #include "sleep.h"
 
+void usage()
+{
+    printf("Usage: sleep number\nExample: sleep 5");
+}
+
+void sleep(const char *arg)
+{
+    int n = atoi(arg);
+    Sleep(n*1000);
+}
+
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+        usage();
+    else
+        sleep(argv[1]);
     return (0);
 }
--- sleep.h
+++ sleep.h
@@ -1,1 +1,4 @@
 #pragma once
+
+#include <windows.h>
+#include <cstdio>
